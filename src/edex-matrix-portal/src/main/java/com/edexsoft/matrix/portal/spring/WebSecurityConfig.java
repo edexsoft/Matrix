8101ef146fdff6d3c304cffa26e3a6c601686ac3
matrix.static <matrix.static@gmail.com>
2016-12-02 15:47:59 +0800

    add method security and taglib

--- src/edex-matrix-portal/src/main/java/com/edexsoft/matrix/portal/spring/WebSecurityConfig.java
+++ src/edex-matrix-portal/src/main/java/com/edexsoft/matrix/portal/spring/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.edexsoft.matrix.portal.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -29,9 +30,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
 //		ShaPasswordEncoder encoder = new ShaPasswordEncoder();
 //	    auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
-		// auth.userDetailsService(userDetailsService);
-		// auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
 		auth.authenticationProvider(edexAuthenticationProvider);
+//		auth
+//			.inMemoryAuthentication()
+//				.withUser("user").password("password").roles("USER");
+//		auth
+//			.jdbcAuthentication()
+//		        .dataSource(dataSource)
+//		        .withDefaultSchema()
+//		        .withUser("user").password("password").roles("USER").and()
+//		        .withUser("admin").password("password").roles("USER", "ADMIN");
+//		auth
+//	        .ldapAuthentication()
+//	            .userDnPatterns("uid={0},ou=people")
+//	            .groupSearchBase("ou=groups");
 	}
 
 //	@Bean
@@ -62,8 +74,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-//				.antMatchers("/", "/home").permitAll()
-//				.antMatchers("/account/login").permitAll()
 				.antMatchers("/api/root/**").access("hasRole('ROLE_ADMIN')")
 				.antMatchers("/root/**").access("hasRole('ROLE_ADMIN')")
 				.antMatchers("/member/**").access("hasRole('ROLE_MEMBER')")
@@ -82,10 +92,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.logout()
 				.logoutUrl("/account/logout")
 				.logoutSuccessUrl("/account/login")
+//				.logoutSuccessHandler(logoutSuccessHandler)
+//	            .invalidateHttpSession(true)
+//	            .addLogoutHandler(logoutHandler)
+//	            .deleteCookies(cookieNamesToClear)
 		.and()
 			.exceptionHandling()
 			.accessDeniedPage("/Access_Denied")
 //		.and().httpBasic()
+//		.and()
+//			.withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
+//                public <O extends FilterSecurityInterceptor> O postProcess(
+//                        O fsi) {
+//                    fsi.setPublishAuthorizationSuccess(true);
+//                    return fsi;
+//                }
+//            })
 		.and()
 			.csrf().disable();
 	}
--- src/edex-matrix-portal/src/main/webapp/areas/web/account/login.jsp
+++ src/edex-matrix-portal/src/main/webapp/areas/web/account/login.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <!DOCTYPE html>
 <html>
@@ -33,6 +33,13 @@
         <!-- /.login-logo -->
         <div class="login-box-body">
             <p class="login-box-msg">登录</p>
+            
+			<c:if test="${param.error != null}">
+	            <p>Invalid username and password.</p>
+	        </c:if>
+	        <c:if test="${param.logout != null}">
+	            <p>You have been logged out.</p>
+	        </c:if>
 
             <form action="/account/login2?r=x" method="post">
                 <div class="form-group has-feedback">
